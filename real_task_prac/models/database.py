f893ef99fca2d32affb4fb293a42a1a3d85a91f4
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-04-16 12:36:13 +0300

    Update database.py
    Перевод на асинхронные рельсы

--- real_task_prac/models/database.py
+++ real_task_prac/models/database.py
@@ -1,15 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase
 from real_task_prac.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
-class BaseModel(DeclarativeBase):
+class BaseModel(AsyncAttrs, DeclarativeBase):
     pass
 
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+async_engine = create_async_engine(DATABASE_URL, pool_pre_ping=True)
 
-Session = sessionmaker(bind=engine)
+AsyncSession = async_sessionmaker(bind=async_engine, class_=AsyncSession)
