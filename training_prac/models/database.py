cffa6d4bb3c549c2ab531aeb6d66262df88cf866
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-04-16 12:37:19 +0300

    Update database.py

--- training_prac/models/database.py
+++ training_prac/models/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from training_prac.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 class BaseModel(DeclarativeBase):
