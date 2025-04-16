b9c153cdaa964cee37b1b83d13720fbcb47c2402
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-04-16 12:37:06 +0300

    UrlParser переписан на асинхронную версию

--- real_task_prac/parsers/url_parser.py
+++ real_task_prac/parsers/url_parser.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
 import requests
+import aiohttp
+from aiohttp import ClientSession, ClientConnectorError
 from bs4 import Tag, BeautifulSoup
+from fake_useragent import UserAgent
 
 from real_task_prac.config import URL, REPORTING_START_DATE
 
@@ -12,43 +15,47 @@ class UrlParser:
         self.default_url = URL + 'markets/oil_products/trades/results/'
 
     @staticmethod
-    def get_bulletins_date(link: Tag):
+    async def get_bulletins_date(link: Tag):
         date = link.parent.parent.find('span').text
         datetime_format = datetime.strptime(date, "%d.%m.%Y")
         return datetime_format
 
-    @staticmethod
-    def get_data_from_site(url: str) -> BeautifulSoup | None:
-        response = requests.get(url)
-        if response.status_code == 404:
-            return None
-        return BeautifulSoup(response.text, 'html.parser')
+    async def get_data_from_site(self, url: str) -> BeautifulSoup | None:
+        try:
+            async with aiohttp.ClientSession() as sess:
+                async with sess.get(url, headers={'User-Agent': UserAgent().random}) as response:
+                    if response.status == 404:
+                        return None
+                    return BeautifulSoup(await response.text(), 'html.parser')
+        except ClientConnectorError:
+            print("Ошибка соединения, пробую еще раз")
+            return await self.get_data_from_site(url)
 
-    def get_links(self, soup: BeautifulSoup, break_flag: bool = False) -> tuple[list[str], bool]:
+    async def get_links(self, soup: BeautifulSoup, break_flag: bool = False) -> tuple[list[str], bool]:
         result = []
         # Поиск по ссылкам на xls файлы с определенным текстом заголовка ссылки
         links = [link for link in soup.select('a.accordeon-inner__item-title.link.xls') if
                  link.text == "Бюллетень по итогам торгов в Секции «Нефтепродукты»"]
         for link in links:
-            if self.get_bulletins_date(link) < REPORTING_START_DATE:  # Проверка даты документа
-                print("Остановка парсинга, найдена дата", self.get_bulletins_date(link))
+            if await self.get_bulletins_date(link) < REPORTING_START_DATE:  # Проверка даты документа
+                print("Остановка парсинга, найдена дата", await self.get_bulletins_date(link))
                 break_flag = True
                 break
             result.append(link.get('href'))
         return result, break_flag
 
-    def parse(self) -> list[str]:
+    async def parse(self) -> list[str]:
         result = []
         page = 1
         while True:
             # Пока цикл не получит break_flag, будет обрабатывать новые страницы
             print(f"Парсинг страницы #{page}")
-            soup = self.get_data_from_site(f'{self.default_url}?page=page-{page}')
+            soup = await self.get_data_from_site(f'{self.default_url}?page=page-{page}')
             if soup is None:
                 # Если в status_code пришел 404 - остановка работы. На сайте кончились страницы.
                 print(f"Страница № {page} не найдена. Остановка работы")
                 break
-            links, break_flag = self.get_links(soup)
+            links, break_flag = await self.get_links(soup)
             result.extend(links)
             if break_flag is True:
                 break
@@ -56,7 +63,7 @@ class UrlParser:
         return result
 
     @staticmethod
-    def download_by_link(link):
-        response = requests.get(URL + link)
-        return response
-
+    async def download_by_link(link, session: ClientSession):
+        async with session.get(URL + link, headers={'User-Agent': UserAgent().random}) as response:
+            if response.status == 200:
+                return await response.read()
