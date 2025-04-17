5431e53a771cc2fba31f1ad59650ba451d31f1a3
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2025-04-17 14:56:19 +0300

    Update main.py
    Добавлен метод для вставки данных в бд. Интерфейс адаптирован под новую логику работы file_processor.py

--- real_task_prac/main.py
+++ real_task_prac/main.py
@@ -1,11 +1,31 @@
 import asyncio
+from datetime import datetime
 from time import time
+
+from pandas import DataFrame
 
 from real_task_prac.config import DATE_FORMAT
 from real_task_prac.file_processor import FileProcessor
 from real_task_prac.models import Result
 from real_task_prac.models.database import BaseModel, async_engine, AsyncSession
 from real_task_prac.parsers.url_parser import UrlParser
+
+
+async def insert_bulletin(table: DataFrame, date: datetime, session: AsyncSession, file_processor: FileProcessor):
+    async for dto in file_processor.get_rows(table):
+        result_data = {
+            'exchange_product_id': dto.exchange_product_id,
+            'exchange_product_name': dto.exchange_product_name,
+            'oil_id': dto.oil_id,
+            'delivery_basis_id': dto.delivery_basis_id,
+            'delivery_basis_name': dto.delivery_basis_name,
+            'delivery_type_id': dto.delivery_type_id,
+            'volume': dto.volume,
+            'total': dto.total,
+            'count': dto.count,
+            'date': date
+        }
+        session.add(Result(**result_data))
 
 
 async def main():
@@ -17,31 +37,8 @@ async def main():
     urls = await url_parser.parse()
     file_processor = FileProcessor(urls, url_parser)
     await file_processor.write_files()
-    tables = await file_processor.iterate_files()
-
-    start_insert_time = time()
-    for key in sorted(tables.keys()):
-        # Для каждого отчета создается своя транзакция
-        async with AsyncSession() as sess:
-            print(f"Запись отчета за {key.strftime(DATE_FORMAT)}")
-            new_rows = []
-            async for dto in file_processor.get_rows(tables[key]):
-                result_data = {
-                    'exchange_product_id': dto.exchange_product_id,
-                    'exchange_product_name': dto.exchange_product_name,
-                    'oil_id': dto.oil_id,
-                    'delivery_basis_id': dto.delivery_basis_id,
-                    'delivery_basis_name': dto.delivery_basis_name,
-                    'delivery_type_id': dto.delivery_type_id,
-                    'volume': dto.volume,
-                    'total': dto.total,
-                    'count': dto.count,
-                    'date': key
-                }
-                new_rows.append(Result(**result_data))
-            sess.add_all(new_rows)
-            await sess.commit()
-    print(f"Загрузка в базу данных завершена за {time() - start_insert_time}")
+    async with AsyncSession() as session:
+        await file_processor.iterate_files(insert_bulletin, session)
 
 
 if __name__ == '__main__':
